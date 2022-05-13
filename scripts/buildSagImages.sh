8d1166a3d0a36f19d2933b76d3a6cbf51cd6a623
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-05-13 17:49:33 +0200

    Beautify 2

--- scripts/buildSagImages.sh
+++ scripts/buildSagImages.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 
-echo "Build Software AG images"
-echo "DEBUG - dumping environment"
-env | sort
+. ${BUILD_SOURCESDIRECTORY}/scripts/setEnv.sh
 
-# local variables
-SUIF_TAG="v.0.0.3-temp"
-d=$(date +%y-%m-%dT%H.%M.%S_%3N)
-crtDay=$(date +%y-%m-%d)
-wd="/tmp/work_$d" # our work directory
-sd="/tmp/share"   # share directory - images
-binDir="$sd/bin"
-installerSharedBin="$binDir/installer.bin"
-sumBootstrapSharedBin="$binDir/sum-bootstrap.bin"
-# templates is a space delimited list of SUIF templates to compute the images for"
-templates="UM/1011/RealmServer UM/1011/TemplateApplications MSR/1011/lean"
-# templates="MSR/1011/lean"
+# TODO: get the variable group
 
-ob
-SUIF_exports(){
-  export SUIF_AUDIT_BASE_DIR=/tmp/SUIF_AUDIT
-  export SUIF_DEBUG_ON=1
-  export SUIF_FIX_IMAGES_OUTPUT_DIRECTORY="/tmp/fixes"
-  export SUIF_FIX_IMAGES_SHARED_DIRECTORY="$sd/fixes"
-  export SUIF_FIXES_DATE_TAG="$crtDay"
-  export SUIF_HOME=/tmp/SUIF
-  export SUIF_INSTALL_INSTALLER_BIN=/tmp/installer.bin
-  export SUIF_PATCH_SUM_BOOSTSTRAP_BIN=/tmp/sum-bootstrap.bin
-  export SUIF_PRODUCT_IMAGES_OUTPUT_DIRECTORY="/tmp/products"
-  export SUIF_PRODUCT_IMAGES_PLATFORM="LNXAMD64"
-  export SUIF_PRODUCT_IMAGES_SHARED_DIRECTORY="$sd/products"
-  export SUIF_SDC_ONLINE_MODE=1 # tell SUIF we are downloading from SDC
-  export SUIF_SUM_HOME=/tmp/sumv11
-}
-SUIF_exports
 
 getSUIF(){
   mkdir -p "${SUIF_HOME}" "${SUIF_AUDIT_BASE_DIR}"
@@ -68,57 +38,61 @@ env | grep SUIF_ | sort
 # }
 # updateMachine
 
-if [ ! -f "${SECUREINFO_SECUREFILEPATH}" ]; then
-  echo "Secure file path not present: ${SECUREINFO_SECUREFILEPATH}"
+if [ ! -f "${SDCCREDENTIALS_SECUREFILEPATH}" ]; then
+  echo "Secure file path not present: ${SDCCREDENTIALS_SECUREFILEPATH}"
   exit 1
 fi
 
 logI "Sourcing secure information..."
-chmod u+x "${SECUREINFO_SECUREFILEPATH}"
-. "${SECUREINFO_SECUREFILEPATH}"
+chmod u+x "${SDCCREDENTIALS_SECUREFILEPATH}"
+. "${SDCCREDENTIALS_SECUREFILEPATH}"
 
 if [ -z ${SAG_AZ_SA_NAME+x} ]; then
   echo "Secure information has not been sourced correctly"
   exit 2
 fi
 
-mountImagesShare(){
-  logI "Mounting the given file share"
-  mkdir -p "$sd"
-  sudo mount -t cifs "$AZ_SMB_PATH" "$sd" -o "vers=3.0,username=$SAG_AZ_SA_NAME,password=$AZ_SM_SHARE_KEY,dir_mode=0777,file_mode=0777"
-  resultMount=$?
-  if [ $resultMount -ne 0 ]; then
-    logE "Error mounting the images share, result $resultMount"
-    exit 4
-  fi
-  logI "Creating work folder and assuring shared folders (${binDir})"
-  mkdir -p "${binDir}" "$wd" "$sd/sessions/$crtDay"
-  touch "${binDir}/lastMountTime"
-}
-mountImagesShare
+# mountImagesShare(){
+#   logI "Mounting the given file share"
+#   mkdir -p "$sd"
+#   sudo mount -t cifs "$AZ_SMB_PATH" "$sd" -o "vers=3.0,username=$SAG_AZ_SA_NAME,password=$AZ_SM_SHARE_KEY,dir_mode=0777,file_mode=0777"
+#   resultMount=$?
+#   if [ $resultMount -ne 0 ]; then
+#     logE "Error mounting the images share, result $resultMount"
+#     exit 4
+#   fi
+#   logI "Creating work folder and assuring shared folders (${binDir})"
+#   mkdir -p "${binDir}" "$wd" "$sd/sessions/$crtDay"
+#   touch "${binDir}/lastMountTime"
+# }
+# mountImagesShare
+
+logI "Creating work folder and assuring shared folders (${MY_binDir})"
+mkdir -p "${MY_binDir}" "$MY_wd" "$MY_sd/sessions/$MY_crtDay"
+touch "${MY_binDir}/lastMountTime"
 
 assureBinaries(){
-  if [ -f "${installerSharedBin}" ]; then
+  if [ -f "${MY_installerSharedBin}" ]; then
     logI "Copying installer binary from the share"
-    cp "${installerSharedBin}" "${SUIF_INSTALL_INSTALLER_BIN}"
+    cp "${MY_installerSharedBin}" "${SUIF_INSTALL_INSTALLER_BIN}"
     logI "Installer binary copied"
   else
     logI "Downloading default SUIF installer binary"
     assureDefaultInstaller
     logI "Copying installer binary to the share"
-    cp "${SUIF_INSTALL_INSTALLER_BIN}" "${installerSharedBin}"
+    cp "${SUIF_INSTALL_INSTALLER_BIN}" "${MY_installerSharedBin}"
     logI "Installer binary copied, result $?"
   fi
 
-  if [ -f "${sumBootstrapSharedBin}" ]; then
+  if [ -f "${MY_sumBootstrapSharedBin}" ]; then
     logI "Copying sum bootstrap binary from the share"
-    cp "${sumBootstrapSharedBin}" "${SUIF_PATCH_SUM_BOOSTSTRAP_BIN}"
-    logI "Installer binary copied"
+    cp "${MY_sumBootstrapSharedBin}" "${SUIF_PATCH_SUM_BOOSTSTRAP_BIN}"
+    logI "SUM bootstrap binary copied"
   else
     logI "Downloading default SUIF installer binary"
     assureDefaultSumBoostrap
     logI "Copying sum bootstrap to the share"
-    cp "${SUIF_PATCH_SUM_BOOSTSTRAP_BIN}" "${sumBootstrapSharedBin}"
+    cp "${SUIF_PATCH_SUM_BOOSTSTRAP_BIN}" "${MY_sumBootstrapSharedBin}"
     logI "SUM Bootstrap binary copied, result $?"
   fi
   chmod u+x "${SUIF_INSTALL_INSTALLER_BIN}"
@@ -132,6 +106,7 @@ assureSUM(){
 }
 assureSUM
 
+# TODO: move these in SUIF commons
 # Parameters
 # $1 -> setup template
 generateFixesImageFromTemplate(){
@@ -290,12 +265,5 @@ processTemplates(){
 
 processTemplates
 
-finally(){
-  logI "Saving the audit"
-  tar cvzf "$sd/sessions/$crtDay/s_$d.tgz" "${SUIF_AUDIT_BASE_DIR}"
-  logI "Unmounting the shared images folder"
-  sudo umount "$sd"
-  logI "Unmounted, result is $?"
-}
-finally
-# TODO: work in progress
+logI "Saving the audit"
+tar cvzf "$MY_sd/sessions/$MY_crtDay/s_$MY_d.tgz" "${SUIF_AUDIT_BASE_DIR}"
