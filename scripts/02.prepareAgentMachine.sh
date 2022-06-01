329e233ab995090d8bde2945180839d188c4a418
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-06-01 23:08:33 +0200

    Refactor 2022-06-01

---
+++ scripts/02.prepareAgentMachine.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. ${BUILD_SOURCESDIRECTORY}/scripts/setEnv.sh
+
+if [ ! -f ". ${SUIF_HOME}/01.scripts/commonFunctions.sh" ]; then
+  echo "SUIF not downloaded correctly!"
+  exit 1
+fi
+
+. "${SUIF_HOME}/01.scripts/commonFunctions.sh"
+
+logI "Updating OS software"
+sudo apt update
+#sudo apt install cifs-utils samba-common samba winbind
+logI "Installing required libraries"
+sudo apt install cifs-utils wget apt-transport-https software-properties-common
+logI "Installing powershell"
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+# Install PowerShell
+sudo apt-get install -y powershell
+logI "Machine prepared successfully"
--- scripts/setEnv.sh
+++ scripts/setEnv.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Pipeline parameters
+export MY_SUIF_TAG=${MY_SUIF_TAG:-"main"} # Expect this to be a pipeline parameter
+export MY_templates=${MY_templates:-"MSR/1011/lean"}
 
 # local variables
 export MY_d=$(date +%y-%m-%dT%H.%M.%S_%3N)
 export MY_crtDay=$(date +%y-%m-%d)
-export MY_wd="/tmp/work" # our work directory
 export MY_sd="/tmp/share"   # share directory - images
 export MY_binDir="$MY_sd/bin"
 export MY_installerSharedBin="$MY_binDir/installer.bin"
@@ -13,7 +14,9 @@ export MY_sumBootstrapSharedBin="$MY_binDir/sum-bootstrap.bin"
 
 # SUIF exports
 export SUIF_AUDIT_BASE_DIR=/tmp/SUIF_AUDIT
-export SUIF_DEBUG_ON=1
+# ATTN: SUIF_DEBUG_ON=1 produces more logs, but may leave some functions waiting for user input
+#       do not use it for unattended "final" forms of the pipelines
+export SUIF_DEBUG_ON=${SUIF_DEBUG_ON:-0} # Expect this to be a pipeline parameter
 export SUIF_FIX_IMAGES_OUTPUT_DIRECTORY="/tmp/fixes"
 export SUIF_FIX_IMAGES_SHARED_DIRECTORY="$MY_sd/fixes"
 export SUIF_FIXES_DATE_TAG="$MY_crtDay"
