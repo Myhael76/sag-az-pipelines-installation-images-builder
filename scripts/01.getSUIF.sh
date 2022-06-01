82db3ba6238074cbb5ac42d1f72995321f77abdc
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-06-01 23:45:25 +0200

    debug 2022-06-01 1

--- scripts/01.getSUIF.sh
+++ scripts/01.getSUIF.sh
@@ -6,6 +6,9 @@ echo "Environment dump before SUIF download"
 env | sort
 
 mkdir -p "${SUIF_HOME}" "${SUIF_AUDIT_BASE_DIR}"
+
+echo "Cloning tag ${MY_SUIF_TAG} of SUIF"
+
 git clone -b "${MY_SUIF_TAG}" --single-branch https://github.com/SoftwareAG/sag-unattented-installations.git "${SUIF_HOME}"
 if [ $? -ne 0 ]; then
   echo "ERROR downloading SUIF"
@@ -15,3 +18,7 @@ if [ ! -f "${SUIF_HOME}/01.scripts/commonFunctions.sh" ]; then
   echo "SUIF clone unseccessful, cannot continue"
   exit 2
 fi
+
+cat "${SUIF_HOME}/01.scripts/installation/setupFunctions.sh"
+
+exit 3
