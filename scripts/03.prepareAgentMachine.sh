d7ffd604910b1b66ec73a790d81fccb5bca902b1
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-06-06 15:04:07 +0200

    Forgotten some -y ...

--- scripts/03.prepareAgentMachine.sh
+++ scripts/03.prepareAgentMachine.sh
@@ -4,16 +4,16 @@
 . "${SUIF_HOME}/01.scripts/commonFunctions.sh"
 
 logI "Updating OS software"
-sudo apt update
+sudo apt -y update
 
 logI "Installing required libraries"
-sudo apt install -y cifs-utils wget apt-transport-https software-properties-common
+sudo apt -y install cifs-utils wget apt-transport-https software-properties-common
 logI "Installing powershell"
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
 # Update the list of packages after we added packages.microsoft.com
-sudo apt-get update
+sudo apt-get -y update
 # Install PowerShell
-sudo apt-get install -y powershell
+sudo apt-get -y install powershell
 logI "Machine prepared successfully"
